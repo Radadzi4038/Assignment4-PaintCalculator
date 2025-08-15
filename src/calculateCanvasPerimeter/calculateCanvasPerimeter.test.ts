3e7672afcb2e32893d985b848bdd2658516dd926
Radadzi4038 <radadzi4038@conestogac.on.ca>
2025-08-14 20:05:09 -0400

    test: add failing test for negative input handling in calculateCanvasPerimeter

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
@@ -1,1 +1,9 @@
 import calculateCanvasPerimeter from './calculateCanvasPerimeter';
+
+describe("calculateCanvasPerimeter", () => {
+  test("handles negative numbers as absolute values", () => {
+    // RED: currently fails if we don't handle abs correctly
+    expect(calculateCanvasPerimeter("-10", "20")).toEqual(60);
+  });
+  
+});
